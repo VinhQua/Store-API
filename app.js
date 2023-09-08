04f235bf5819fd8d718654ac7ce09cf79f4edf7c
Nguyen Vinh Quang <123441916+VinhQua@users.noreply.github.com>
2023-09-08 15:38:05 +0700

    new ch

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static("./public"));
 app.use(express.json());
 //routes
 
-app.get("/").send(`    <h1>Store <a href="api/v1/products">API</a></h1>`)
+app.get("/",(req,res)=>send(`    <h1>Store <a href="api/v1/products">API</a></h1>`))
 
 //product routes
 app.use("/api/v1/products", products);
